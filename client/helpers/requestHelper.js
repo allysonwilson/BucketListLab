2de4d058d594e4ead4264e615f369595636f3b37
Benjamin Conway <benrconway84@gmail.com>
2017-10-25 13:52:54 +0100

    add helpers

---
+++ client/helpers/requestHelper.js
@@ -0,0 +1,29 @@
+var requestHelper = {
+  getRequest: function (url, fishingLine) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.addEventListener('load', function(){
+      var data = JSON.parse(this.responseText);
+      fishingLine(data);
+    })
+
+    xhr.send();
+  }
+
+  post: function ( url, fishingLine, info) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    var stringToSubmit = JSON.stringify(info);
+
+    xhr.addEventListener('load' , function() {
+      var data = JSON.parse(this.responseText);
+      fishingLine(data);
+    })
+
+    xhr.send(stringToSubmit);
+  }
+}
+
+module.exports = requestHelper;
